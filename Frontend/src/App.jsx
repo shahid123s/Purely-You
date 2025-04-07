c0b11a6e9e147260e35ff47e3db00f9e4e8bf2fd
Benson B Varoor <bensonbaiju46@gmail.com>
2025-04-07 21:05:06 +0530

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./Pages/HomePage"
 import PatientRoutes from "./routes/PatientRoutes"
 import DoctorRoutes from "./routes/DoctorRoutes"
 import AdminRoutes from "./routes/AdminRoutes"
+import AISearch from "./pages/AiSearch"
+import TeamPage from "./pages/team/TeamPage"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="/admin/*" element={<AdminRoutes />} />
       <Route path="/doctor/*" element={<DoctorRoutes />} />
       <Route path="/patient/*" element={<PatientRoutes />} />
+      <Route path="/team" element={<TeamPage/>}/>
+      <Route path="/ai" element={<AISearch/>} />
       <Route path="/" element={<HomePage />} />
     </Routes>
   )
